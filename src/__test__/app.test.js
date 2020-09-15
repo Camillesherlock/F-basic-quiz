80bd5baa7cabc62dfb8a7ca6d3a49beea9f4ea4b
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-15 22:43:48 +0800

    [lk] add some test

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,3 +1,17 @@
-test("add 1+1 is 2", () => {
-  expect(2).toBe(2);
+import { TestScheduler } from "jest";
+import { getInformation, getEducation } from "../request";
+
+jest.mock("../request")
+describe("getInformation",()=>{
+test("should fetch data when called getInformation", async()=>{
+  getInformation.mockImplementation(()=>{});
+  getInformation(1);
+  expect(getInformation).toHaveBeenCalled();
 });
+
+test("should fetch education data when called getEducation",async()=>{
+  getEducation.mockImplementation(()=>{});
+  getEducation(1);
+  expect(getInformation).toHaveBeenCalled();
+})
+})
